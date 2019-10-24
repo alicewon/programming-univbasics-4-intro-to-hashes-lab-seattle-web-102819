c78c0e8b21121404d895a21b82e5e72e4c82457a
Alice Won <alicewon4@gmail.com>
2019-10-24 06:59:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,6 +42,7 @@ def update_counting_hash(hash, key)
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
 
+  
   if update_counting_hash[key]
       update_counting_hash[key] +=1
   else

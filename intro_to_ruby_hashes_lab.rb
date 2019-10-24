c5351f3630991758e4adaa01e8f984ce4ae979ef
Alice Won <alicewon4@gmail.com>
2019-10-24 07:21:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -38,13 +38,14 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
+  if hash[key]
+      hash[key] +=1
+  else
+    hash[key] = 1
+    hash
+  end
+
+
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
-
-  if update_hash[key]
-      update_hash[key] +=1
-  else
-    update_hash[key] = 1
-    update_hash
-  end
